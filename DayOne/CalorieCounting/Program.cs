7da2b653b1ec2c4acdeaa7dac1bba0bb3828f4fa
Fawwaaz777 <118934189+Fawwaaz777@users.noreply.github.com>
2023-06-19 22:55:59 +0400

    Day one part 2 solution

--- DayOne/CalorieCounting/Program.cs
+++ DayOne/CalorieCounting/Program.cs
@@ -1,4 +1,5 @@
 ﻿string[] lines = File.ReadAllLines("./Input/input.txt");
+Console.WriteLine(lines.Length);
 
 int[] array = new int[lines.Length];
 int currentIndex = 0;
@@ -20,6 +21,23 @@ int maxValue = 0;
 for (int i = 0; i < array.Length; i++)
 {
     if (array[i] > maxValue)
-    { maxValue = array[i]; maxIndex = i; }
+    { maxValue = array[i]; 
+    maxIndex = i; }
 }
-Console.WriteLine("Index with highest value: " + maxIndex);
+Console.WriteLine("Index with highest value: " + maxIndex + " And Max Calorie =" + maxValue);
+
+Dictionary<int, int> dict = array.Select((value, index) => new { index, value })
+                                     .ToDictionary(x => x.index, x => x.value);
+
+var top3 = dict.OrderByDescending(x => x.Value).Take(3);
+Console.WriteLine("Top 3 values");
+foreach (var item in top3)
+{
+    Console.WriteLine($"{item.Key} -> {item.Value}");
+}
+int total = 0;
+foreach (var item in top3)
+{
+    total += item.Value;
+}
+Console.WriteLine($"Top3Total -> {total}");
